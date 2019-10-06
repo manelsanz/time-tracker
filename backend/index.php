76b17008c1ee1bb7a747063ed6cbc8c8819fabe5
psanz94 <pol@systemberri.com>
2019-10-06 20:45:35 +0200

    Simple router added.

--- backend/index.php
+++ backend/index.php
@@ -11,8 +11,24 @@ $pdo = new PDO("pgsql:" . sprintf(
     ltrim($db["path"], "/")
 ));
 
-$stmt = $pdo->query("SELECT * FROM tasks ORDER BY id DESC LIMIT 1");
+$task_id = null;
+
+switch ($_SERVER['REQUEST_URI']) {
+    case "/task/1":
+        $task_id = 1;
+
+    case "/task/2":
+        $task_id = 2;
+        break;
+    default:
+        header('HTTP/1.0 404 Not Found');
+        break;
+}
+
+$stmt = $pdo->query("SELECT * FROM tasks WHERE id=?");
+$stmt->execute([$task_id]);
 $task = $stmt->fetch();
 
 // echo var_dump($task);
-echo json_encode($var_dump);
+header('Content-type: application/json');
+echo json_encode($task);
