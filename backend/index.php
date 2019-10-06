590751ba72658bdb74fff1908534dc733a50ef92
psanz94 <pol@systemberri.com>
2019-10-06 20:33:35 +0200

    Test db connection.

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "hello world 2";
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$stmt = $pdo->query("SELECT * FROM tasks ORDER BY id DESC LIMIT 1");
+$task = $stmt->fetch();
+
+echo var_dump($task);
